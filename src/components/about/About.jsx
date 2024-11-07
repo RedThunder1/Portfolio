5b3c4974fa588aa433b56bf5f81fd6b669a2ea56
James <redthunderspartan@gmail.com>
2024-11-07 11:24:47 -0600

    Some Changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,16 +4,24 @@ import "./about.css"
 const About = () => {
     return (
         <div className="about" id="about">
-            <h1>About</h1>
-            <div className="about_info">
-                <div className="basic_info">
-                    <p> Welcome to my portfolio page!
-                        My name is James Haver, a student attending the University of Nebraska Omaha pursuing a computer
-                        science degree.
-                        I like to create projects, solve problems, and to learn more. I am experienced in Java, and
-                        Python with intermediate
-                        Knowledge of HTML, JavaScript/TypeScript, and CSS.
-                    </p>
+            <div className="basic_info">
+                <h1>About</h1>
+                <div className="about">
+                    <div className="about_description">
+                        <p> Welcome to my portfolio page!
+                            My name is James Haver, a student attending the University of Nebraska Omaha pursuing a computer
+                            science degree. I like to create projects, solve problems, and to learn more. I am experienced in Java, and
+                            Python with intermediate Knowledge of HTML, JavaScript/TypeScript, and CSS.
+                        </p>
+                    </div>
+                    <div className="skills">
+                        <h2>Skills</h2>
+                        <ul className="skills_list">
+                            <li className="skills_item">
+
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="contact">
                     <h2>Contact Info</h2>
@@ -25,17 +33,12 @@ const About = () => {
                             <p>(402)-637-3739</p>
                         </li>
                         <li className="contact_item">
-                            <a href="https://github.com/RedThunder1" className="contact_link"><i className="icon-social-github"></i></a>
+                            <a href="https://github.com/RedThunder1" className="contact_link"><i
+                                className="icon-social-github"></i></a>
                         </li>
                         <li className="contact_item">
-                            <a href="https://www.linkedin.com/in/james-haver/" className="contact_link"><i className="icon-social-linkedin"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="skills">
-                    <ul className="skills_list">
-                        <li className="skills_item">
-
+                            <a href="https://www.linkedin.com/in/james-haver/" className="contact_link"><i
+                                className="icon-social-linkedin"></i></a>
                         </li>
                     </ul>
                 </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,18 +4,12 @@ import Logo from '../../assets/ThunderBox.png'
 
 const Portfolio = () => {
     return (
-        <div className="about" id="portfolio">
+        <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <div className="projects">
                 <div className="thunderbox_project">
-                    <h2>ThunderBox</h2>
-                    <img src={Logo} className="thunderbox_logo" alt="project_logo"/>
-                    <a href="https://github.com/RedThunder1/ThunderBox"><i className="icon-social-github"></i></a>
-                    <p className="description">ThunderBox is a project of mine where I am developing a web music player.</p>
+
                 </div>
-                <div className="project2"></div>
-                <div className="project3"></div>
-                <div className="project4"></div>
             </div>
         </div>
     )
