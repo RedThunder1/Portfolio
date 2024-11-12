acd9886c41df27cbd6521429e59df11706cc0e92
James <redthunderspartan@gmail.com>
2024-11-11 19:10:04 -0600

    Openable and Closable sidebar.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import "./sidebar.css"
-import Logo from "../../assets/Logo.png"
 
 function opencloseSidebar() {
     const sidebar = document.getElementById("sidebar");
-    console.log("clicked");
+    const main = document.getElementById("main");
     if (sidebar.style.width !== "100px") {
         sidebar.style.width = "100px";
+        main.style.width = "calc(100vw - 100px)"
     } else {
         sidebar.style.width = "0px";
+        main.style.width = "100%";
     }
 }
 
 const Sidebar = () => {
     return (
-        <aside className="sidebar" id="sidebar">
+        <aside>
             <a onClick={opencloseSidebar} className="sidebar_button">
-                <img src={Logo} alt="Logo" className="sidebar_logo" draggable="false"/>
+                <i className="icon-menu"/>
             </a>
-            <nav className="nav">
-                <div className="nav__menu">
-                    <ul className="nav__list">
+            <div className="sidebar" id="sidebar">
+                <nav className="nav">
+                    <div className="nav__menu">
+                        <ul className="nav__list">
 
-                        <li className="nav__item">
-                            <a href="javascript:document.getElementById('home').scrollIntoView({behavior: 'smooth'})"
-                               className="nav__link">
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="javascript:document.getElementById('about').scrollIntoView({behavior: 'smooth'})"
-                               className="nav__link">
-                                <i className="icon-user"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="javascript:document.getElementById('portfolio').scrollIntoView({behavior: 'smooth'})"
-                               className="nav__link">
-                                <i className="icon-wrench"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+                            <li className="nav__item">
+                                <a href="javascript:document.getElementById('home').scrollIntoView({behavior: 'smooth'})"
+                                   className="nav__link">
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="javascript:document.getElementById('about').scrollIntoView({behavior: 'smooth'})"
+                                   className="nav__link">
+                                    <i className="icon-user"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="javascript:document.getElementById('portfolio').scrollIntoView({behavior: 'smooth'})"
+                                   className="nav__link">
+                                    <i className="icon-wrench"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </aside>
     )
 }
